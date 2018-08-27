912c0c274e304f714bb7d62937c40d392357a75d
Chough <carter.hough@hotmail.com>
2018-08-27 14:51:17 -0600

    Added more examples in index.php

--- PHP-Docker-Test/index.php
+++ PHP-Docker-Test/index.php
@@ -1,6 +1,24 @@
 <?php
 $name = "Carter";
+$name = 3;
+$count = 32;
+$names = array(700111222=>"alice", 700333444=>"bob", 700555666=>"cindy");
+$x = 3.1415;
+
+function sayHi($who)
+{
+	echo "<br>Hi $who!\n";
+}
 ?>
 <html>
-	Hello, World
+	Hi,
+	<?php
+	foreach ($names as $id=>$name)
+	{
+		echo "<br>$id: ";
+		sayHi($name);
+	}
+
+	echo "<br><br>x=$x\n";
+	?>
 </html>
