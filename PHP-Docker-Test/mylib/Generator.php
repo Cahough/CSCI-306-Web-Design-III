ec3c184244d1bfd2253aab555541225b9ae50ad9
Chough <carter.hough@hotmail.com>
2018-08-31 11:13:38 -0600

    Refactored code formatting

--- PHP-Docker-Test/mylib/Generator.php
+++ PHP-Docker-Test/mylib/Generator.php
@@ -1,7 +1,9 @@
 <?php
-namespace mylib;     //Generator was a keyword, so had to make own namespace.
-class Generator{    //base class for anything that creates output
-    public function generate(){
+namespace mylib;     // Generator was a keyword, so had to make own namespace.
+class Generator      // base class for anything that creates output
+{
+    public function generate()
+    {
         echo "generating";
     }
 }
--- PHP-Docker-Test/mylib/Page.php
+++ PHP-Docker-Test/mylib/Page.php
@@ -1,40 +1,57 @@
 <?php
 namespace mylib;
 require_once "Generator.php";       //better to use than include, dies if cannot find it
-class Page extends Generator {  //php.net inheritance page for reference
-    public function doctype(){
+class Page extends Generator       //php.net inheritance page for reference
+{
+    public function doctype()
+    {
         echo "<!DOCTYPE= html>";
     }
-    public function html(){
+
+    public function html()
+    {
         echo "<html>\n";
         $this->head();
         $this->body();
         echo "</html>\n";
     }
-    public function page(){
+    public function page()
+    {
         $this->doctype();
         $this->html();
     }
-    public function head(){
+
+    public function head()
+    {
         echo "<title>";
         $this->title();
         echo "</title>";
         $this->js();
         $this->css();
     }
-    public function js(){
+
+    public function js()
+    {
         echo "<script></script>";
     }
-    public function css(){
+
+    public function css()
+    {
         echo "<style></style>";
     }
-    public function title(){
+
+    public function title()
+    {
         echo "This is the title";
     }
-    public function body(){
+
+    public function body()
+    {
         echo "<body>This is the body.</body>";
     }
-    public function generate(){
+
+    public function generate()
+    {
         $this->page();
     }
 }
